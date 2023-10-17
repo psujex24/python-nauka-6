8feee11f19d25e0a25456c3bbcc417b1fa134bc9
Psujex24 <kubakubakuba113@gmail.com>
2023-10-17 15:58:55 +0200

    test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from random import randint
 
-for i in range(100):
-    print(randint(1,10))
-    
+los = randint(1,10)
+odp = -1
+i = 0
+print("Zgadnij liczbę z przedziału 1 - 10")
+
+while odp != los:
+    i += 1
+    odp = int(input("Podaj liczbę: "))
+    if odp > los:
+        print("Niestety, wylosowana liczba jest mniejsza od Twojej")
+    elif odp < los:
+        print("Niestety, wylosowana liczba jest większa od Twojej")
+print("Brawo! Odgadłeś za", i, "razem.")
